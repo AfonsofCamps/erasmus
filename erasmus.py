b179e54cdf9b975f290b3a7945e7fbf9495e5b3c
AfonsofCamps <afonsofcampos08@gmail.com>
2025-10-03 12:38:01 +0100

    Update erasmus.py

--- erasmus.py
+++ erasmus.py
@@ -976,3 +976,4 @@ if __name__ == '__main__':
     print("🎨 Templates criados (templates/).")
     print("🔐 Admin: username='admin', password='admin123' — altera em produção!")
     app.run(debug=True, host='0.0.0.0', port=5000)
+
